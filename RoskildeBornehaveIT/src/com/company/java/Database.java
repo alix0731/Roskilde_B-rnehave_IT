393c8f70806ca7a771f2e8eae00d333e97a6426c
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-03-26 22:32:41 +0100

    Update Database.java

--- RoskildeBornehaveIT/src/com/company/java/Database.java
+++ RoskildeBornehaveIT/src/com/company/java/Database.java
@@ -1,4 +1,25 @@
 package com.company.java;
 
 public class Database {
+
+    String url = "jdbc:mysql://den1.mysql4.gear.host/roskildeit123";
+    String user = "roskildeit123";
+    String password = "Ce5AC91-?j0u";
+
+    public void createSchedule(){
+
+    }
+
+    public void getSchedule(){
+
+    }
+
+    public void deleteSchedule(){
+
+    }
+
+    public void updateSchedule(){
+
+    }
+
 }
